9d0fcf2d92f9093d9ee284c386687ae4252147e5
mhkeller <michael.keller@gmail.com>
2015-03-01 22:37:07 -0500

    init2

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@
 	// Load the data from the json file. This path is relative to our `main.js` file so we have to use the double dots, `..` to go back down a directory and then into our data folder.
 	$.getJSON('../data/ebola-totals-by-country.json', function(ebolaData){
 
-
+	});
 
 }).call(this);
