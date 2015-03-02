9d25d3d05e1ebbc121bfb137a6125d179f2b5a04
mhkeller <michael.keller@gmail.com>
2015-03-01 22:53:08 -0500

    btns

--- js/main.js
+++ js/main.js
@@ -5,27 +5,46 @@
 	$.getJSON('../data/ebola-totals-by-country.json', function(ebolaData){
 
 		var country_graph = 	'<div class="country-group">' +
-														'<div class="country-name"></div>' +
-														'<div class="cases-bar"><div class="label"></div></div>' +
+														'<div class="name"></div>' +
+														'<div class="bar"><div class="label"></div></div>' +
 													'</div>';
 
-		var max_cases = _.max(ebolaData, function(d){ return d.cases_total; }).cases_total;
+		var max_cases = _.max(ebolaData, function(d){ return d.cases; }).cases;
 
-		console.log(max_cases)
+		console.log(max_cases);
 
 		ebolaData.forEach(function(country){
 			var $country_graph = $(country_graph);
-			$country_graph.find('.country-name').html(country.name)
-			$country_graph.find('.cases-bar').css({
-				width: (country.cases_total / max_cases)*100 + '%'
+			$country_graph.find('.name').html(country.name)
+			$country_graph.find('.bar').css({
+				width: (country.cases / max_cases)*100 + '%'
 			});
 
-			$country_graph.find('.label').html(country.cases_total);
+			$country_graph.find('.label').html(country.cases);
 
 			$('#canvas').append($country_graph);
 		});
 
 
+		function updateBars(series){
+			$('.country-group').each(function(index, el){
+				var $country_group = $(el);
+
+				$country_group.find('.bar').css({
+					width: (ebolaData[index][series] / max_cases)*100 + '%'
+				});
+
+			$country_group.find('.label').html(ebolaData[index][series]);
+
+			});
+		}
+
+		$('.js-button').on('click', function(){
+			var series = $(this).attr('data-which-series');
+			updateBars(series);
+		});
+
 	});
+
 
 }).call(this);
